618b93d2d8b2804533008c8d56b0e3bdbce30a45
Justin Brooks <justin@jzbrooks.com>
2024-02-29 09:30:46 -0500

    Initial Commit

---
+++ ReducerConfusion/ReducerConfusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReducerConfusionApp.swift
+//  ReducerConfusion
+//
+//  Created by Justin Brooks on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ReducerConfusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
